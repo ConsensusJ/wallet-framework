4a53b908a76fcea2d74fed1d2ace2a68e8d13ce0
Sean Gilligan <sean@msgilligan.com>
2019-05-17 12:04:02 -0700

    Create a subproject for the Airgap network wallet

---
+++ airgapfx-wallet/src/main/java/airgapfxwallet/AirgapFxMainWindowController.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright by the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package airgapfxwallet;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.walletfx.OverlayableWindowController;
+import org.bitcoinj.walletfx.WalletFxApp;
+import org.bitcoinj.walletfx.WalletMainWindowController;
+import org.bitcoinj.walletfx.WalletSettingsController;
+import org.bitcoinj.walletfx.controls.ClickableBitcoinAddress;
+import org.bitcoinj.walletfx.controls.NotificationBarPane;
+import org.bitcoinj.walletfx.utils.easing.EasingMode;
+import org.bitcoinj.walletfx.utils.easing.ElasticInterpolator;
+
+import javax.inject.Singleton;
+
+import com.blockchaincommons.airgap.fx.camera.CameraService;
+import com.blockchaincommons.airgap.fx.components.CameraView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ */
+@Singleton
+public class AirgapFxMainWindowController extends WalletMainWindowController {
+    private static final Logger log = LoggerFactory.getLogger(AirgapFxMainWindowController.class);
+    @FXML private HBox controlsBox;
+    @FXML private Label balance;
+    @FXML private Button sendMoneyOutBtn;
+    @FXML private Button scanBtn;
+    @FXML private ClickableBitcoinAddress addressControl;
+
+    protected final AirgapFxWalletApp app;
+
+    public AirgapFxMainWindowController(AirgapFxWalletApp app) {
+        super(app);
+        this.app = app;
+    }
+
+        // Called by FXMLLoader.
+    public void initialize() {
+        addressControl.setOpacity(0.0);
+    }
+
+
+    public void onBitcoinSetup() {
+        super.onBitcoinSetup();
+        addressControl.addressProperty().bind(model.addressProperty());
+        balance.textProperty().bind(createBalanceStringBinding(model.balanceProperty()));
+        // Don't let the user click send money when the wallet is empty.
+        sendMoneyOutBtn.disableProperty().bind(model.balanceProperty().isEqualTo(Coin.ZERO));
+        // Don't let the user click the "scan QR" button if there is no camera
+        var cameraAvailable = app.cameraService != null;
+        scanBtn.setDisable(!cameraAvailable);
+    }
+
+
+    @FXML
+    private void sendMoneyOut(ActionEvent event) {
+        // Hide this UI and show the send money UI. This UI won't be clickable until the user dismisses send_money.
+        overlayUI("send_money.fxml");
+    }
+
+    @FXML
+    private void settingsClicked(ActionEvent event) {
+        OverlayableWindowController.OverlayUI<WalletSettingsController> screen = overlayUI("wallet_settings.fxml");
+        screen.controller.initialize(null);
+    }
+
+    @FXML
+    public void scanClicked(ActionEvent actionEvent) {
+        log.info("scanClicked");
+        CameraService cameraService = app.cameraService;
+        //cameraService.addQRListener(this::resultListener);
+
+        Button startStop = new Button();
+        startStop.textProperty()
+                .bind(Bindings
+                        .when(cameraService.runningProperty())
+                        .then("Stop")
+                        .otherwise("Start"));
+
+        startStop.setOnAction(e -> {
+            if (cameraService.isRunning()) {
+                cameraService.cancel();
+            } else {
+                cameraService.restart();
+            }
+        });
+
+        CameraView view = new CameraView(cameraService);
+
+        BorderPane root = new BorderPane(view.getView());
+        BorderPane.setAlignment(startStop, Pos.CENTER);
+        BorderPane.setMargin(startStop, new Insets(5));
+        root.setBottom(startStop);
+
+        Scene scene = new Scene(root);
+        final Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void restoreFromSeedAnimation() {
+        // Buttons slide out ...
+        TranslateTransition leave = new TranslateTransition(Duration.millis(1200), controlsBox);
+        leave.setByY(80.0);
+        leave.play();
+    }
+
+    @Override
+    protected void readyToGoAnimation() {
+        // Buttons slide in and clickable address appears simultaneously.
+        TranslateTransition arrive = new TranslateTransition(Duration.millis(1200), controlsBox);
+        arrive.setInterpolator(new ElasticInterpolator(EasingMode.EASE_OUT, 1, 2));
+        arrive.setToY(0.0);
+        FadeTransition reveal = new FadeTransition(Duration.millis(1200), addressControl);
+        reveal.setToValue(1.0);
+        ParallelTransition group = new ParallelTransition(arrive, reveal);
+        group.setDelay(NotificationBarPane.ANIM_OUT_DURATION);
+        group.setCycleCount(1);
+        group.play();
+    }
+}
