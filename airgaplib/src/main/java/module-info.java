c7327fe0b7d1182e5703e86d8b6731775ae58be8
Sean Gilligan <sean@msgilligan.com>
2019-10-27 13:05:41 -0700

    Remove unused Guava dependency from airgap lib

--- airgaplib/src/main/java/module-info.java
+++ airgaplib/src/main/java/module-info.java
@@ -7,9 +7,7 @@ module org.consensusj.airgap {
     requires com.fasterxml.jackson.core;
     requires jackson.annotations;
     requires org.slf4j;
-
-    requires com.google.common;
-
+    
     exports org.consensusj.airgap;
     exports org.consensusj.airgap.json;
     exports org.consensusj.airgap.keychain;
