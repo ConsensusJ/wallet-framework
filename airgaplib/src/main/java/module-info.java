7208e1d10cf79d5346b72ca5ccb0de0347b668e6
Sean Gilligan <sean@msgilligan.com>
2020-03-11 10:16:50 -0700

    Bump airgaplib back to Java 9 with module-info

--- airgaplib/src/main/java/module-info.java
+++ airgaplib/src/main/java/module-info.java
@@ -1,11 +1,11 @@
 /**
- * Unused Module Info file moved here after we downgraded this module to JDK 8
+ * Java Platform Module System dependencies and exports for airgaplib.
+ * Note: We may backport this library to Java 8 and eliminate this file.
  */
 module org.consensusj.airgap {
     requires org.bitcoinj.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
-    requires jackson.annotations;
     requires org.slf4j;
     
     exports org.consensusj.airgap;
